a2ec8609b344cb3befa0ab1216bc6293c681c532
nextendweb <nextendweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-05 12:58:46 +0000

    = 1.4.37 = * Added check for login inputs
    git-svn-id: https://plugins.svn.wordpress.org/nextend-google-connect/trunk@648314 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nextend-google-connect.php
+++ nextend-google-connect.php
@@ -3,7 +3,7 @@
 Plugin Name: Nextend Google Connect
 Plugin URI: http://nextendweb.com/
 Description: Google connect
-Version: 1.4.36
+Version: 1.4.37
 Author: Roland Soos
 License: GPL2
 */
@@ -314,7 +314,8 @@ function new_add_google_login_form(){
         has_social_form = true;
         var loginForm = $.merge($('#loginform'),$('#registerform'));
         socialLogins = $('<div class="newsociallogins" style="text-align: center;"><div style="clear:both;"></div></div>');
-        loginForm.prepend("<h3 style='text-align:center;'>OR</h3>");
+        if(loginForm.find('input').lenght > 0)
+          loginForm.prepend("<h3 style='text-align:center;'>OR</h3>");
         loginForm.prepend(socialLogins);
       }
       if(!window.google_added){
